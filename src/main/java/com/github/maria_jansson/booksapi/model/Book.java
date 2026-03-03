da1ac0bb9e6f1dbf9c42c2d9e88c0ebddc151113
Maria Jansson <mj225er@student.lnu.se>
2026-03-03 14:06:54 +0100

    Update relationships and add some getters, plus basic setter signatures

--- src/main/java/com/github/maria_jansson/booksapi/model/Book.java
+++ src/main/java/com/github/maria_jansson/booksapi/model/Book.java
@@ -1,6 +1,10 @@
 package com.github.maria_jansson.booksapi.model;
 
+import com.github.maria_jansson.booksapi.dto.AuthorDTO;
+import com.github.maria_jansson.booksapi.dto.CategoryDTO;
 import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Book {
@@ -8,14 +12,74 @@ public class Book {
     @GeneratedValue
     private Long id;
     private String title;
-    @ManyToOne
-    private Author author;
+    @ManyToMany
+    private List<Author> authors;
     private String description;
-    @ManyToOne
-    private Category category;
+    @ManyToMany
+    private List<Category> categories;
     private String publisher;
     private double price;
     private String publishMonth;
     private int publishYear;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Author> getAuthors() {
+        return authors;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getPublishMonth() {
+        return publishMonth;
+    }
+
+    public int getPublishYear() {
+        return publishYear;
+    }
+
+    public void setTitle(String title) {
+    }
+
+    public void setAuthors(List<Author> longs) {
+    }
+
+    public void setDescription(String description) {
+    }
+
+    public void setCategories(List<Category> longs) {
+    }
+
+    public void setPublisher(String publisher) {
+    }
+
+    public void setPrice(Double price) {
+    }
+
+    public void setPublishMonth(String s) {
+    }
+
+    public void setPublishYear(int i) {
+    }
 
 }
