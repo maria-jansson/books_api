bd17fde14afb43876c52d59a0739597660b67775
Maria Jansson <mj225er@student.lnu.se>
2026-03-03 15:55:19 +0100

    Add getters and setters via Lombok instead of manually

--- src/main/java/com/github/maria_jansson/booksapi/model/Book.java
+++ src/main/java/com/github/maria_jansson/booksapi/model/Book.java
@@ -3,9 +3,13 @@ package com.github.maria_jansson.booksapi.model;
 import com.github.maria_jansson.booksapi.dto.AuthorDTO;
 import com.github.maria_jansson.booksapi.dto.CategoryDTO;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 public class Book {
     @Id
@@ -21,65 +25,5 @@ public class Book {
     private double price;
     private String publishMonth;
     private int publishYear;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public List<Author> getAuthors() {
-        return authors;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public List<Category> getCategories() {
-        return categories;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public String getPublishMonth() {
-        return publishMonth;
-    }
-
-    public int getPublishYear() {
-        return publishYear;
-    }
-
-    public void setTitle(String title) {
-    }
-
-    public void setAuthors(List<Author> longs) {
-    }
-
-    public void setDescription(String description) {
-    }
-
-    public void setCategories(List<Category> longs) {
-    }
-
-    public void setPublisher(String publisher) {
-    }
-
-    public void setPrice(Double price) {
-    }
-
-    public void setPublishMonth(String s) {
-    }
-
-    public void setPublishYear(int i) {
-    }
 
 }
